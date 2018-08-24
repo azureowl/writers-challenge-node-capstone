fa375a042e9dfb633252ca890adb7dcc0ec0bca9
Azureowl <maiaroche16@gmail.com>
2018-08-24 15:34:02 -0700

    Expandable pages menu for mobile

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,20 @@
 (function () {
     'use strict';
 
+    function getPages (id) {
+        return $('.pages section').filter(`[data-book=${id}]`);
+    }
+
     function toggleCollapseMenu () {
         $('.nav').on('click', '.expandable', function (e) {
-            const target = $(this).next();
+            let target = $(this).next();
+            const id = $(this).find('button').attr('id');
             // use expanded variable to set the state of the target
             const expanded = $(this).find('button').attr('aria-expanded') === 'true' || false;
             $(this).find('button').attr('aria-expanded', !expanded);
+            if (id !== undefined) {
+                target = getPages(id);
+            }
             target.attr('hidden', expanded);
         });
     }
