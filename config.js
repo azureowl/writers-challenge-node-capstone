815a0bd27954808a9ec9886bc1db74d50687b869
Azureowl <maiaroche16@gmail.com>
2018-08-27 13:06:13 -0700

    Styled login button

--- config.js
+++ config.js
@@ -1,4 +1,29 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://admin:password1@ds131902.mlab.com:31902/writers-challenge';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-    'mongodb://admin:password1@ds131902.mlab.com:31902/writers-challenge';
-exports.PORT = process.env.PORT || 8080;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://admin:password1@ds131902.mlab.com:31902/writers-challenge';
+// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+//     'mongodb://admin:password1@ds131902.mlab.com:31902/writers-challenge';
+// exports.PORT = process.env.PORT || 8080;
