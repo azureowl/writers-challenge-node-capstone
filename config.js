5a7c880600e7f279be53027d2043238c151e58b5
Azureowl <maiaroche16@gmail.com>
2018-08-27 21:14:43 -0700

    Set up server functions

--- config.js
+++ config.js
@@ -1,29 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://admin:password1@ds131902.mlab.com:31902/writers-challenge';
-// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-//     'mongodb://admin:password1@ds131902.mlab.com:31902/writers-challenge';
-// exports.PORT = process.env.PORT || 8080;
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:password1@ds131902.mlab.com:31902/writers-challenge';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://admin:password1@ds133152.mlab.com:33152/writers-challenge-test';
+exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -1,14 +1,50 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.Promise = global.Promise;
 
-
-const PORT = process.env.PORT || 3000;
+const {PORT, DATABASE_URL, TEST_DATABASE_URL} = require('./config');
 
 app.use(morgan('common'));
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}.`);
-});
+let server;
+
+function runServer(TEST_DATABASE_URL, PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(TEST_DATABASE_URL, {useNewUrlParser: true}, err => {
+        if (err) {
+            return reject(`Something went wrong -- MongoError: ${err.errmsg}`);
+        }
+        server = app.listen(PORT, () => {
+            console.log(`Your app is listening on port ${PORT}`);
+            resolve('Successfully connected!');
+        }).on('error', (err) => {
+            mongoose.disconnect();
+            reject(`Whoops! there was an error: ${err}.`);
+        });
+    }).catch(err => {
+        console.log(`Whoops! there was an error: ${err.name}: ${err.errmsg}.`);
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+    runServer(TEST_DATABASE_URL, PORT).catch(err => console.error(err));
+}
