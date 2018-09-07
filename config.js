605e07d9b7df50ef550a63da05c44af756d7302d
Azureowl <maiaroche16@gmail.com>
2018-09-06 19:30:08 -0700

    Removed toString method

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ require('dotenv').config();
 //   OXKEY: process.env.OXKEY
 // };
 
-exports.DATABASE_URL = process.env.DATABASE_URL.toString();
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL.toString();
+exports.DATABASE_URL = process.env.DATABASE_URL;
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
 exports.PORT = process.env.PORT || 8080;
 exports.OXID = process.env.OXID;
 exports.OXKEY = process.env.OXKEY;
