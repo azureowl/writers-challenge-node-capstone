02e43a9a04cb355a9453432d34440a31842f569f
Azureowl <maiaroche16@gmail.com>
2018-09-06 19:34:02 -0700

    set explicit string for test url

--- config.js
+++ config.js
@@ -1,15 +1,7 @@
 require('dotenv').config();
 
-// const config = {
-//   db: process.env.DATABASE_URL,
-//   testdb: process.env.TEST_DATABASE_URL,
-//   port: process.env.PORT,
-//   OXID: process.env.OXID,
-//   OXKEY: process.env.OXKEY
-// };
-
 exports.DATABASE_URL = process.env.DATABASE_URL;
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://admin:password1@ds133152.mlab.com:33152/writers-challenge-test';
 exports.PORT = process.env.PORT || 8080;
 exports.OXID = process.env.OXID;
 exports.OXKEY = process.env.OXKEY;
